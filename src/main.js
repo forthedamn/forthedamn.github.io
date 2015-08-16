a66d94acb8a82d3ec00b33008de4ef22b35f9c4c
zhongguoxin <zhongguoxin@meituan.com>
2015-08-16 11:44:25 +0800

    modified:   CNAME

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ ThemeManager = new mui.Styles.ThemeManager(),
 AppBar = mui.AppBar,
 FontIcon = mui.FontIcon,
 IconButton = mui.IconButton;
-
+var Card = mui.Card,
+CardHeader = mui.CardHeader,
+CardMedia = mui.CardMedia,
+CardTitle = mui.CardTitle,
+CardActions = mui.CardActions,
+CardText = mui.CardText,
+FlatButton = mui.FlatButton,
+Avatar = mui.Avatar;
 
 var MyCard = React.createClass({
     childContextTypes: {
@@ -17,11 +24,30 @@ var MyCard = React.createClass({
     },
     render: function() {
        return (
+        <div>
            <AppBar
                 title="Xin"
                 //iconClassNameRight="muidocs-icon-navigation-expand-more"
-                iconElementRight={<FontIcon className="muidocs-icon-action-home" />}
+                iconElementRight={<IconButton iconClassName="muidocs-icon-custom-github" tooltip="GitHub"/>}
+                style={{position:"fixed",top:"0"}}
                 />
+            <Card style={{width:"50%",margin:"80px auto",position:"relative"}}>
+               <CardHeader
+                 title="first article coming to boom!"
+                 subtitle="start to write"
+                 avatar={<Avatar>Xin</Avatar>}/>
+               <CardMedia overlay={<CardTitle title="Title" subtitle="Subtitle"/>}>
+                 <img src="http://lorempixel.com/600/337/nature/"/>
+               </CardMedia>
+               <CardTitle title="Title" subtitle="Subtitle"/>
+               <CardActions>
+                 <FlatButton label="评论"/>
+               </CardActions>
+               <CardText>
+                 还是准备写点东西，做下读书笔记
+               </CardText>
+             </Card>
+        </div>
        );
     }
 });
